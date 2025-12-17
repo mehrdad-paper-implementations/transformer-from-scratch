b853cdc740b40eff5f88fc43f8070ebf66833b9e
Mehrdad <ndr.mehrdad@gmail.com>
2025-12-17 22:58:55 +0330

    Add Transformer Encoder Layer and edit the multi-head-attention inputs

--- src/multi_head_attention.py
+++ src/multi_head_attention.py
@@ -14,27 +14,29 @@ class MultiHeadAttention(nn.Module):
         self.d_k = d_model // n_heads
         self.d_v = d_v if d_v is not None else self.d_k
 
+        # Get q, k and v by applying linear layer
         self.weights_q = nn.Linear(d_model, d_model, bias=False)
         self.weights_k = nn.Linear(d_model, d_model, bias=False)
-        self.weights_v = nn.Linear(d_model, self.d_v * self.n_heads, bias=False)
+        self.weights_v = nn.Linear(d_model, d_v * self.n_heads, bias=False)
 
         self.spda = ScaledDotProductAttention(self.d_k)
 
         self.projection = nn.Linear(self.d_v * self.n_heads, d_model, bias=False)
 
-    def forward(self, x, mask=None):
+    def forward(self, q, k, v, mask=None):
         """
         q_shape: [B, T, D]
         """
+
         # Get q, k and v by applying linear layer
-        q = self.weights_q(x)
-        k = self.weights_k(x)
-        v = self.weights_v(x)
+        q = self.weights_q(q)
+        k = self.weights_k(k)
+        v = self.weights_v(v)
 
         # Reshape to adding head dimension
-        q = q.view(x.shape[0], x.shape[1], self.n_heads, self.d_k)  # [B, T, H, d_k]
-        k = k.view(x.shape[0], x.shape[1], self.n_heads, self.d_k)  # [B, T, H, d_k]
-        v = v.view(x.shape[0], x.shape[1], self.n_heads, self.d_v)  # [B, T, H, d_v]
+        q = q.view(q.shape[0], q.shape[1], self.n_heads, self.d_k)  # [B, T, H, d_k]
+        k = k.view(k.shape[0], k.shape[1], self.n_heads, self.d_k)  # [B, T, H, d_k]
+        v = v.view(v.shape[0], v.shape[1], self.n_heads, self.d_v)  # [B, T, H, d_v]
 
         # Permute the H and T because of SDPA works on two last dimensions
         q = q.permute(0, 2, 1, 3)  # [B, H, T, d_k]
@@ -46,10 +48,11 @@ class MultiHeadAttention(nn.Module):
 
         # Apply scaled-dot-product-attention
         attentions = self.spda(q, k, v, mask)  # [B, H, T, d_v]
+        B, _, T, _ = attentions.shape
 
         # Permute to reverse changes
-        attentions = attentions.permute(0, 2, 1, 3).contiguous().view(x.shape[0], x.shape[1], self.d_v * self.n_heads)  # [B, T, D]
+        attentions = attentions.permute(0, 2, 1, 3).contiguous().view(B, T, self.d_v * self.n_heads)  # [B, T, D]
 
         output = self.projection(attentions)  # [B, T, D]
 
-        return output
+        return output  # [B, T, D]
---
+++ src/transformer_encoder_layer.py
@@ -0,0 +1,49 @@
+"""
+Transformer Encoder Layer
+"""
+from torch import nn
+from multi_head_attention import MultiHeadAttention
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, n_heads, d_model, d_ff, d_v=None, dropout=0.1):
+        super().__init__()
+
+        self.n_heads = n_heads
+        self.d_model = d_model
+
+        self.d_k = d_model // n_heads
+        self.d_v = d_v if d_v is not None else self.d_k
+
+        self.multi_head_attention = MultiHeadAttention(self.n_heads, self.d_model, d_v)
+
+        self.intermediate_layer_norm = nn.LayerNorm(d_model)
+        self.final_layer_norm = nn.LayerNorm(d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.feed_forward_module = nn.Sequential(
+            nn.Linear(d_model, d_ff, bias=True),
+            nn.ReLU(),
+            nn.Linear(d_ff, d_model, bias=True))
+
+    def forward(self, x, padding_mask=None):
+        """
+        x_shape: [B, T, D]
+        padding_mask: [B, T]
+        """
+
+        # Apply multi-head-attention
+        output = self.multi_head_attention(x, x, x, padding_mask)  # [B, T, D]
+
+        # Add and norm
+        output = self.dropout(output)
+        normed_output = self.intermediate_layer_norm(x + output)
+
+        # Feed-Forward
+        output = self.feed_forward_module(normed_output)
+
+        # Add and norm
+        normed_output = self.final_layer_norm(output + normed_output)
+
+        return normed_output
