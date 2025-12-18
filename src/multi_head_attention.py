Add simple forward sanity test and fix masking bugs

src/multi_head_attention.py
@@ -2,7 +2,7 @@
 Multi-head-attention class
 """
 from torch import nn
-from scaled_dot_product_attention import ScaledDotProductAttention
+from .scaled_dot_product_attention import ScaledDotProductAttention
 
 
 class MultiHeadAttention(nn.Module):
@@ -17,7 +17,7 @@ class MultiHeadAttention(nn.Module):
         # Get q, k and v by applying linear layer
         self.weights_q = nn.Linear(d_model, d_model, bias=False)
         self.weights_k = nn.Linear(d_model, d_model, bias=False)
-        self.weights_v = nn.Linear(d_model, d_v * self.n_heads, bias=False)
+        self.weights_v = nn.Linear(d_model, self.d_v * self.n_heads, bias=False)
 
         self.spda = ScaledDotProductAttention(self.d_k)
 

src/transformer.py
@@ -1,9 +1,8 @@
 import math
-
 from torch import nn
-from transformer_encoder_block import TransformerEncoderBlock
-from transformer_decoder_block import TransformerDecoderBlock
-from positional_encoding import AbsolutePositionalEncoding
+from .transformer_encoder_block import TransformerEncoderBlock
+from .transformer_decoder_block import TransformerDecoderBlock
+from .positional_encoding import AbsolutePositionalEncoding
 
 
 class Transformer(nn.Module):
@@ -26,7 +25,8 @@ class Transformer(nn.Module):
         self.d_emb = d_emb if d_emb is not None else d_model
 
         if self.d_model != self.d_emb:
-            self.project_emb_to_d_model = nn.Linear(d_emb, d_model)
+            self.decoder_project_emb_to_d_model = nn.Linear(d_emb, d_model)
+            self.encoder_project_emb_to_d_model = nn.Linear(d_emb, d_model)
             self.project_d_model_to_emb = nn.Linear(d_model, d_emb)
 
         self.encoder_embedder = nn.Embedding(input_vocab_size, d_emb)
@@ -43,16 +43,20 @@ class Transformer(nn.Module):
         self.project_to_vocab_size.weight = self.decoder_embedder.weight
 
     def forward(self, input_ids, output_ids, causal_mask, input_mask=None, output_mask=None):
-        x_emb = self.encoder_embedder(input_ids) * math.sqrt(self.d_emb)
+        x_emb = self.encoder_embedder(input_ids) / math.sqrt(self.d_emb)
 
         if self.d_model != self.d_emb:
-            x_emb = self.project_emb_to_d_model(x_emb)
+            x_emb = self.encoder_project_emb_to_d_model(x_emb)
 
         x_emb = self.encoder_positional_encoding(x_emb)
 
         memory = self.encoder(x_emb, input_mask)
 
-        y_emb = self.decoder_embedder(output_ids) * math.sqrt(self.d_emb)
+        y_emb = self.decoder_embedder(output_ids) / math.sqrt(self.d_emb)
+
+        if self.d_model != self.d_emb:
+            y_emb = self.decoder_project_emb_to_d_model(y_emb)
+
         y_emb = self.decoder_positional_encoding(y_emb)
 
         output = self.decoder(y_emb, memory, causal_mask, output_mask)

src/transformer_decoder_block.py
@@ -2,7 +2,7 @@
 Transformer Decoder Block
 """
 from torch import nn
-from transformer_decoder_layer import TransformerDecoderLayer
+from .transformer_decoder_layer import TransformerDecoderLayer
 
 
 class TransformerDecoderBlock(nn.Module):

src/transformer_decoder_layer.py
@@ -2,7 +2,7 @@
 Transformer Decoder Layer
 """
 from torch import nn
-from multi_head_attention import MultiHeadAttention
+from .multi_head_attention import MultiHeadAttention
 
 
 class TransformerDecoderLayer(nn.Module):

src/transformer_encoder_block.py
@@ -2,7 +2,7 @@
 Transformer Encoder Block
 """
 from torch import nn
-from transformer_encoder_layer import TransformerEncoderLayer
+from .transformer_encoder_layer import TransformerEncoderLayer
 
 
 class TransformerEncoderBlock(nn.Module):

src/transformer_encoder_layer.py
@@ -2,7 +2,7 @@
 Transformer Encoder Layer
 """
 from torch import nn
-from multi_head_attention import MultiHeadAttention
+from .multi_head_attention import MultiHeadAttention
 
 
 class TransformerEncoderLayer(nn.Module):